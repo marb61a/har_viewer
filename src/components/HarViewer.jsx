03e4ae4de65b3c2f87091efdf66f8ac9619b8917
martin byrne <marb61a@gmail.com>
2017-08-25 11:57:36 +0000

    updated dependencie

--- src/components/HarViewer.jsx
+++ src/components/HarViewer.jsx
@@ -9,7 +9,15 @@ const Column = FixedDataTable.Column;
 export default class HarViewer extends React.Component {
     constructor(){
         super();
-        this.state = {};
+        this.state = {
+            columnWidths: {
+                url: 500,
+                size: 100,
+                time: 200
+            },
+            tableWidth: 1000,
+            tableHeight: 500
+        };
     }    
     
     render(){
@@ -22,6 +30,15 @@ export default class HarViewer extends React.Component {
                         </PageHeader>
                     </Col>
                 </Row>  
+                <Row>
+                    <Col sm={12}>
+                        <Table>
+                            <Column dataKey="url" width={this.state.columnWidths.url} isResizable={true} label="Url" />
+                            <Column dataKey="size" width={this.state.columnWidths.size} isResizable={true} label="Size" />
+                            <Column dataKey="time" width={this.state.columnWidths.time} isResizable={true} label="Timeime" />
+                        </Table>
+                    </Col>
+                </Row> 
             </Grid>    
         );
     }
