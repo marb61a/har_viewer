24d6df73dcbc0545501799226a0e69127d413fb0
martin byrne <marb61a@gmail.com>
2017-09-01 16:35:43 +0000

    added sample hars

---
+++ src/samples.js
@@ -0,0 +1,12 @@
+window.samples = [
+    {
+        id: 'so',
+        har: require("./store/sample-hars/stackoverflow.com.har.json"),
+        label: 'Stack Overflow'
+    },
+    {
+        id: 'ind',
+        har: require("./store/sample-hars/independent.ie.har.json"),
+        label: 'Irish Independent'        
+    }
+];
